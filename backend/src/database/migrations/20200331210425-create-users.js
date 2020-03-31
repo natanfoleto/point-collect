76db4242a43260291007c9b6c1f8c648e85ab6a8
Natan Foleto <39577730+natanfoleto@users.noreply.github.com>
2020-03-31 19:08:51 -0300

    Test sequelize

---
+++ backend/src/database/migrations/20200331210425-create-users.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTERGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      location : {
+        type: {
+          type: Sequelize.STRING,
+          enum: ['Point'],
+          allowNull: false,
+        },
+        index: '2dsphere',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  }
+};
