5eacf8ee92c7b8f2476c9faaf4f87e142cbef864
Natan Foleto <39577730+natanfoleto@users.noreply.github.com>
2020-04-02 19:57:21 -0300

    Update maps

--- mobile/src/pages/Maps/index.js
+++ mobile/src/pages/Maps/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Image } from 'react-native'
-import MapView, { Marker } from 'react-native-maps';
+import { Image, View, Text, TextInput, TouchableOpacity } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import styles from './styles'
 
@@ -36,10 +37,31 @@ export default function Maps() {
   }
 
   return (
+    <>
     <MapView initialRegion={currentRegion} style={styles.map}>
       <Marker coordinate={{ latitude: -20.697764, longitude: -48.416833 }}>
-        
+        <Image style={styles.marker} />
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.be}>Você está aqui!</Text>
+            <Text style={styles.point}>O local mais próximo do seu endereço é uma Cooperativa a 34,2km</Text>
+          </View>
+        </Callout>
       </Marker>
     </MapView>
+    <View style={styles.searchForm}>
+      <TextInput 
+        style={styles.searchInput}
+        placeholder="Buscar pontos por cidade..."
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false} 
+      />
+
+      <TouchableOpacity style={styles.loadButton}>
+        <MaterialIcons name="my-location" size={20} color="#fff" />
+      </TouchableOpacity>
+    </View>
+    </>
   );
 }
--- mobile/src/pages/Maps/styles.js
+++ mobile/src/pages/Maps/styles.js
@@ -4,4 +4,63 @@ export default StyleSheet.create({
     map: {
       flex: 1
     },
+
+    marker: {
+      width: 30,
+      height: 30,
+      borderRadius: 50,
+      backgroundColor: '#2173FF',
+      borderWidth: 7,
+      borderColor: 'rgba(205, 220, 250, 0.9)'
+    },
+
+    callout: {
+      width: 260,
+    },
+
+    be: {
+      fontWeight: 'bold',
+      fontSize: 16,
+    },
+
+    point: {
+      color: '#666',
+      marginTop: 6,
+    },
+
+    searchForm: {
+      position: 'absolute',
+      bottom: 20,
+      left: 20,
+      right: 20,
+      zIndex: 5,
+      flexDirection: 'row'
+    },
+
+    searchInput: {
+      flex: 1,
+      height: 50,
+      backgroundColor: '#fff',
+      color: '#333',
+      borderRadius: 25,
+      paddingHorizontal: 20,
+      fontSize: 16,
+      shadowColor: '#000',
+      shadowOpacity: 0.2,
+      shadowOffset: {
+        width: 4,
+        height: 4,
+      },
+      elevation: 2
+    },
+
+    loadButton: {
+      width: 50,
+      height: 50,
+      backgroundColor: '#4BCB56',
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 15
+    }
 });
