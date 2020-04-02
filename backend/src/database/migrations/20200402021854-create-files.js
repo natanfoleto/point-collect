c5dc52e13dee5578b2889884dafa3d2cbc207db6
Natan Foleto <39577730+natanfoleto@users.noreply.github.com>
2020-04-02 00:15:16 -0300

    Associate files with users

--- backend/src/database/migrations/20200402021854-create-files.js
+++ backend/src/database/migrations/20200402021854-create-files.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', { 
+    return queryInterface.createTable('files', { 
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -13,22 +13,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      email: {
+      path: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
-      },
-      password_hash: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      latitude: {
-        type: Sequelize.DECIMAL,
-        allowNull: false,
-      },
-      longitude: {
-        type: Sequelize.DECIMAL,
-        allowNull: false,
       },
       created_at: {
         type: Sequelize.DATE,
@@ -42,6 +30,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('files');
   }
 };
