8303e04010a8f1626c350353c07d1a2d7735a697
Natan Foleto <39577730+natanfoleto@users.noreply.github.com>
2020-04-30 05:24:10 -0300

    Update auth/persist

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve conter mais de 6 digitos')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
-    <h1>Register</h1>
+    <>
+      <img src={logo} alt="GoRecicle"/>
+
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <Input name="nome" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
   );
 }
